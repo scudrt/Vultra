240d3fd57ad77fa3b5c20996bcdfcc982192e5ee
scudrt <1208044562@qq.com>
2024-10-08 05:39:52 +0800

    codes for setting up vulkan (vulkan tutorial page 45)

---
+++ src/VulkanApplication.cpp
@@ -0,0 +1,70 @@
+#include "VulkanApplication.hpp"
+
+void VulkanApplication::run() {
+	initWindow();
+	initVulkan();
+	mainLoop();
+	cleanUp();
+}
+
+void VulkanApplication::initWindow() {
+	glfwInit(); // initializing glfw library
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // don't create opengl context
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	mWindow = glfwCreateWindow(800, 600, "Vultra", nullptr, nullptr);
+}
+
+void VulkanApplication::initVulkan() {
+	createInstance();
+}
+
+void VulkanApplication::createInstance() {
+	/*
+	* Application Info
+	*/
+	VkApplicationInfo appInfo{};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "Hello Triangle";
+	appInfo.pEngineName = "No Engine";
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion = VK_API_VERSION_1_0;
+
+	/*
+	* Instance Create Info
+	*/
+	VkInstanceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+	// getting required extensions
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+	createInfo.enabledExtensionCount = glfwExtensionCount;
+	createInfo.ppEnabledExtensionNames = glfwExtensions;
+	createInfo.enabledLayerCount = 0;
+
+	// Createing vulkan instance
+	if (vkCreateInstance(&createInfo, nullptr, &mInstance) != VK_SUCCESS) {
+		throw std::runtime_error("failed to create vulkan instance");
+	}
+}
+
+void VulkanApplication::mainLoop() {
+	while (!glfwWindowShouldClose(mWindow)) {
+		glfwPollEvents();
+	}
+}
+
+void VulkanApplication::cleanUp() {
+	/*
+	* Clean Up Window
+	*/
+	glfwDestroyWindow(mWindow);
+	glfwTerminate(); // corresponding to glfwInit()
+
+	/*
+	* Clean Up vulkan
+	*/
+	;
+}
---
+++ src/VulkanApplication.hpp
@@ -0,0 +1,21 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <stdexcept>
+
+struct VulkanApplication {
+public:
+	void run();
+private:
+	void initWindow();
+
+	void initVulkan();
+	void createInstance();
+
+	void mainLoop();
+
+	void cleanUp();
+
+	GLFWwindow* mWindow;
+	VkInstance mInstance;
+};
