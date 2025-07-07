950e87b343fbdc6ef21202c9e69d48895d1648e0
scudrt <1208044562@qq.com>
2025-07-08 02:40:18 +0800

    pick up my project Vultra after a long time

--- src/VulkanApplication.cpp
+++ src/VulkanApplication.cpp
@@ -3,6 +3,11 @@
 #include <iostream>
 #include <vector>
 
+VulkanApplication::VulkanApplication(int screenWdith, int screenHeight)
+: mScreenWidth(screenWdith)
+, mScreenHeight(screenHeight){
+}
+
 void VulkanApplication::run() {
 	initWindow();
 	initVulkan();
@@ -14,34 +19,43 @@ void VulkanApplication::initWindow() {
 	glfwInit(); // initializing glfw library
 
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // don't create opengl context
-	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-	mWindow = glfwCreateWindow(800, 600, "Vultra", nullptr, nullptr);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // we don't support resizing for now
+	mWindow = glfwCreateWindow(mScreenWidth, mScreenHeight, "Vultra", nullptr, nullptr);
 }
 
 void VulkanApplication::initVulkan() {
+	checkExtensionSupport();
 	createInstance();
 }
 
-void VulkanApplication::createInstance() {
+void VulkanApplication::checkExtensionSupport() const {
 	/*
 	* Check and show available extensions names
 	*/
 	uint32_t extensionsCount = 0;
+	// Get size of extension properties
 	vkEnumerateInstanceExtensionProperties(nullptr, &extensionsCount, nullptr);
+
+	// Get the array of extension properties
 	std::vector<VkExtensionProperties> extensions(extensionsCount);
 	vkEnumerateInstanceExtensionProperties(nullptr, &extensionsCount, extensions.data());
-	std::cout << "available extensions:" << std::endl;
+
+	std::cout << "Available extensions: " << extensionsCount << std::endl;
 	for (const auto& extension : extensions) {
-		std::cout << extension.extensionName << std::endl;
+		std::cout << '\t' << extension.extensionName << std::endl;
 	}
 
+	// TODO: check if all extensions returned by glfwGetRequiredInstanceExtensions are included in the extensions above
+}
+
+void VulkanApplication::createInstance() {
 	/*
 	* Application Info
 	*/
 	VkApplicationInfo appInfo{};
 	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-	appInfo.pApplicationName = "Hello Triangle";
-	appInfo.pEngineName = "No Engine";
+	appInfo.pApplicationName = "Vultra";
+	appInfo.pEngineName = "Vultra";
 	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
 	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
 	appInfo.apiVersion = VK_API_VERSION_1_0;
@@ -57,9 +71,9 @@ void VulkanApplication::createInstance() {
 	const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 	createInfo.enabledExtensionCount = glfwExtensionCount;
 	createInfo.ppEnabledExtensionNames = glfwExtensions;
-	createInfo.enabledLayerCount = 0;
+	createInfo.enabledLayerCount = 0; // No validation layers for now
 
-	// Createing vulkan instance
+	// Createing vulkan instance finally
 	if (vkCreateInstance(&createInfo, nullptr, &mInstance) != VK_SUCCESS) {
 		throw std::runtime_error("failed to create vulkan instance");
 	}
@@ -73,17 +87,18 @@ void VulkanApplication::mainLoop() {
 
 void VulkanApplication::cleanUp() {
 	/*
-	* Clean Up vulkan
+	* Clean Up vulkan, corresponding to vkCreateInstance()
 	*/
 	if (mInstance != VK_NULL_HANDLE) {
 		vkDestroyInstance(mInstance, nullptr);
 	}
 
 	/*
-	* Clean Up Window
+	* Clean Up Window, corresponding to glfwCreateWindow()
 	*/
 	if (mWindow) {
 		glfwDestroyWindow(mWindow);
 	}
+
 	glfwTerminate(); // corresponding to glfwInit()
 }
--- src/VulkanApplication.hpp
+++ src/VulkanApplication.hpp
@@ -5,16 +5,22 @@
 
 struct VulkanApplication {
 public:
+	VulkanApplication(int screenWdith = 800, int screenHeight = 600);
+
 	void run();
 private:
 	void initWindow();
 
 	void initVulkan();
+	void checkExtensionSupport() const;
 	void createInstance();
 
 	void mainLoop();
 
 	void cleanUp();
+
+	int mScreenWidth = 800;
+	int mScreenHeight = 600;
 
 	GLFWwindow* mWindow = nullptr;
 	VkInstance mInstance = VK_NULL_HANDLE;
